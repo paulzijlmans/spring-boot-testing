28ee1e2cc8bf65160d5e0bc47f5584a9218a2107
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-23 19:58:08 +0200

    Update Web Security

--- src/main/java/nl/paulzijlmans/springboottesting/WebSecurityConfig.java
+++ src/main/java/nl/paulzijlmans/springboottesting/WebSecurityConfig.java
@@ -2,23 +2,24 @@ package nl.paulzijlmans.springboottesting;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeRequests()
         .mvcMatchers(HttpMethod.GET, "/api/customers").permitAll()
         .mvcMatchers(HttpMethod.GET, "/api/customers/*").authenticated()
-        .mvcMatchers(HttpMethod.GET, "/dashboard").permitAll()
         .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
         .anyRequest().authenticated()
         .and()
         .httpBasic();
+    return http.build();
   }
 }
--- src/test/java/nl/paulzijlmans/springboottesting/customer/CustomerControllerTest.java
+++ src/test/java/nl/paulzijlmans/springboottesting/customer/CustomerControllerTest.java
@@ -1,6 +1,5 @@
 package nl.paulzijlmans.springboottesting.customer;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -9,15 +8,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
+import nl.paulzijlmans.springboottesting.WebSecurityConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Import(WebSecurityConfig.class)
 @WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
 
