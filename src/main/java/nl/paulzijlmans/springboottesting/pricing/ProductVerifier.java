5b946273dc01c06f98b85c545a1d6b7b82696590
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-06-23 09:08:28 +0200

    Add Pricing Service

---
+++ src/main/java/nl/paulzijlmans/springboottesting/pricing/ProductVerifier.java
@@ -0,0 +1,7 @@
+package nl.paulzijlmans.springboottesting.pricing;
+
+public class ProductVerifier {
+  public boolean isCurrentlyInStockOfCompetitor(String productName) {
+    return false;
+  }
+}
